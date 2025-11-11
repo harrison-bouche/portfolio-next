adfe98c812ad7236d558f6d4a168de0773166a92
harrison-bouche <bouharri@nmu.edu>
2025-11-10 19:49:11 -0500

    fix: added favicon, fixed ssr, added CDN caching

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
+Route::get('/', function (Request $request) {
+    $res = Inertia::render('Welcome', [
         'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    ])->toResponse($request);
+
+    $res->headers->set(
+        'Cache-Control',
+        'max-age=86400, public, stale-while-revalidate=3600'
+    );
+
+    return $res;
 })->name('home');
 
 Route::get('dashboard', function () {
